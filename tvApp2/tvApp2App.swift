9d91b12b998a216a37f0176c828d02866c4c6c86
David McCarthy <davidmccarthy@Davids-MacBook-Pro-2.local>
2023-03-09 10:29:26 +0000

    This is my first commit

---
+++ tvApp2/tvApp2App.swift
@@ -0,0 +1,18 @@
+//
+//  tvApp2App.swift
+//  tvApp2
+//
+//  Created by David McCarthy on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct tvApp2App: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
